8c77cc397fe7fd50ef4357a67fcfd89715756a1e
zimengfang <weidan0302@126.com>
2016-05-17 10:32:04 +0800

    update something

--- EditableList/app/src/main/java/com/zmf/editablelist/MainActivity.java
+++ EditableList/app/src/main/java/com/zmf/editablelist/MainActivity.java
@@ -7,26 +7,23 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.Window;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<DataEntity> localList = new ArrayList<>();
+    private ArrayList<DataEntity> mList = new ArrayList<>();
     private ListView mListView;
     private MyListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         mListView = (ListView) findViewById(R.id.list_stocks);
 
@@ -44,35 +41,38 @@ public class MainActivity extends AppCompatActivity {
             model.setSecshortname("平安股份" + i);
             model.setTicker("00000" + i);
             model.setSecid("00000" + i + ".XSE");
-            localList.add(model);
+            mList.add(model);
         }
-        mAdapter = new MyListAdapter(localList,MainActivity.this);
+        mAdapter = new MyListAdapter(mList, MainActivity.this);
         mListView.setAdapter(mAdapter);
 
         findViewById(R.id.edit).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, EditListActvity.class);
-                intent.putParcelableArrayListExtra("stocks", localList);
+                intent.putParcelableArrayListExtra("stocks", mList);
                 startActivityForResult(intent, Constant.REQUEST_CODE);
             }
         });
     }
 
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Log.i("stocks", "onActivityResult()");
+        Log.i("editable list", "onActivityResult()");
         switch (resultCode) { //resultCode为回传的标记
             case Constant.REQUEST_CODE:
                 try {
-                    localList.clear();
-                    localList = data.getParcelableArrayListExtra("stocks");
+                    mList.clear();
+                    mList = data.getParcelableArrayListExtra("stocks");
+                    mAdapter = new MyListAdapter(mList, MainActivity.this);
+                    mListView.setAdapter(mAdapter);
                     mAdapter.notifyDataSetChanged();
                     StringBuilder stocks = new StringBuilder();
                     DataEntity entity;
-                    for (int i = 0, p = localList.size(); i < p; i++) {
-                        entity = localList.get(i);
+                    for (int i = 0, p = mList.size(); i < p; i++) {
+                        entity = mList.get(i);
                         if (i == 0) {
                             stocks.append(entity.getSecshortname()).append("&").append(entity.getSecid()).append("&").append(entity.getTicker());
                         } else {
@@ -84,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
                     SharedPreferences.Editor editor = preferences.edit();
                     editor.putString(Constant.SELECTED, stocks.toString());
                     editor.commit();
-                    Log.i("stocks", "onActivityResult() " + stocks.toString());
+                    Log.i("editable list", "onActivityResult() " + stocks.toString());
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -92,27 +92,5 @@ public class MainActivity extends AppCompatActivity {
             default:
                 break;
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
